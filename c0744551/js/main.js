6397f00e61b6ceba3db9dc3496e795ade6721509
visualstudio <visualstudio@230-PC40>
2019-01-31 11:28:30 -0500

    c0744551-Week4

--- c0744551/js/main.js
+++ c0744551/js/main.js
@@ -1,1 +1,10 @@
 ﻿// Your code here!
+//Name: Sheenam Gupta
+//Id: c0744551
+//course: 2214_5
+alert("I love ducks")
+alert(3 + 6)
+alert(4.56578 * 7.2345543)
+var date = new Date()
+document.body.innerHTML = "<h1>Date Today is " + date + "</h1>"
+document.body.innerHTML = "<h1>Date Today is " + date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
