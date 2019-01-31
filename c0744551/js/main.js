2705671f5e3e3780ecdf58f3e5af3880ae0b9835
visualstudio <visualstudio@230-PC40>
2019-01-31 11:46:16 -0500

    calling functions with onload event

--- c0744551/js/main.js
+++ c0744551/js/main.js
@@ -5,6 +5,11 @@
 alert("I love ducks")
 alert(3 + 6)
 alert(4.56578 * 7.2345543)
-var date = new Date()
-document.body.innerHTML = "<h1>Date Today is " + date + "</h1>"
-document.body.innerHTML = "<h1>Date Today is " + date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
+
+
+//code into a function
+function myFunction() {
+    var date = new Date()
+    document.body.innerHTML = "<h1>Date Today is " + date + "</h1>"
+    document.body.innerHTML = "<h1>Date Today is " + date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
+}
